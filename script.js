f34228e93851fe43c50dffa28aade8d0496f8594
maxxNcode <markfroilan2nd@gmail.com>
2026-02-15 14:44:27 +0800

    fixing code spacings

---
+++ script.js
@@ -0,0 +1,9 @@
+function goToRegister() {
+    window.location.href = 'pages/registration.html';
+}
+
+
+// display none for the main content
+document.querySelector('.main').style.display = 'show';
+
+
